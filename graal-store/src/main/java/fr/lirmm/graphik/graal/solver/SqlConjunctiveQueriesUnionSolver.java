0ffa4942da746f47595a97e3a251d230f43fb4e7
Clément SIPIETER <clement.sipieter@lirmm.fr>
2014-07-04 10:00:38 +0200

    SqlConjunctiveQueriesUnionSolver: fix bug on UNION with ;

--- graal-store/src/main/java/fr/lirmm/graphik/graal/solver/SqlConjunctiveQueriesUnionSolver.java
+++ graal-store/src/main/java/fr/lirmm/graphik/graal/solver/SqlConjunctiveQueriesUnionSolver.java
@@ -3,8 +3,10 @@
  */
 package fr.lirmm.graphik.graal.solver;
 
-import java.sql.SQLException;
 import java.util.Iterator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import fr.lirmm.graphik.graal.core.ConjunctiveQueriesUnion;
 import fr.lirmm.graphik.graal.core.ConjunctiveQuery;
@@ -18,6 +20,9 @@ import fr.lirmm.graphik.graal.store.rdbms.ResultSetSubstitutionReader;
  */
 public class SqlConjunctiveQueriesUnionSolver implements Solver {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(SqlConjunctiveQueriesUnionSolver.class);
+	
 	private RdbmsStore store;
 	private StringBuilder sqlQuery;
 	private ConjunctiveQueriesUnion queries;
@@ -34,12 +39,15 @@ public class SqlConjunctiveQueriesUnionSolver implements Solver {
 		try {
 			if (it.hasNext()) {
 				this.sqlQuery.append(this.store.transformToSQL(it.next()));
+				this.sqlQuery.setLength(this.sqlQuery.length() - 1);
 
 				while (it.hasNext()) {
 					this.sqlQuery.append(" UNION ");
 					this.sqlQuery.append(this.store.transformToSQL(it.next()));
+					this.sqlQuery.setLength(this.sqlQuery.length() - 1);
 				}
 			}
+			this.sqlQuery.append(';');
 		} catch (Exception e) {
 			throw new SolverException("Error during query translation to SQL",
 					e);
@@ -55,6 +63,9 @@ public class SqlConjunctiveQueriesUnionSolver implements Solver {
 	public SubstitutionReader execute() throws SolverException {
 		this.preprocessing();
 		try {
+			if(logger.isDebugEnabled()) {
+				logger.debug(this.sqlQuery.toString());
+			}
 			return new ResultSetSubstitutionReader(this.store, this.sqlQuery.toString(), queries.isBoolean());
 		} catch (Exception e) {
 			throw new SolverException(e.getMessage(), e);
