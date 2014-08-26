6d16899288ba6be6d564227ca060002ddfc56d12
Clément SIPIETER <clement.sipieter@lirmm.fr>
2014-08-26 15:15:13 +0200

    add computePieceUnifier function

---
+++ graal-core/src/main/java/fr/lirmm/graphik/util/LinkedSet.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package fr.lirmm.graphik.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+
+/**
+ * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
+ *
+ */
+public class LinkedSet<E> implements Set<E> {
+
+	LinkedList<E> list = new LinkedList<E>();
+	private static final long serialVersionUID = 3056778651225951171L;
+
+	@Override
+	public boolean add(E e) {
+		if(!list.contains(e)) {
+			return list.add(e);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		boolean res = false;
+		for(E e : c)
+			res = res || list.add(e);
+		
+		return res;
+	}
+
+	@Override
+	public void clear() {
+		list.clear();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return list.contains(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return list.containsAll(c);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return list.iterator();
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		return list.remove(o);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		return list.removeAll(c);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return list.retainAll(c);
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return list.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] array) {
+		return list.toArray(array);
+	}
+
+}
