3d26f8dbb43a79090580eee78f770202a41c1eda
Swan Rocher <swan.rocher@lirmm.fr>
2014-09-04 14:42:33 +0200

    fix unbatched atoms

--- graal-store/src/main/java/fr/lirmm/graphik/graal/store/rdbms/AbstractRdbmsStore.java
+++ graal-store/src/main/java/fr/lirmm/graphik/graal/store/rdbms/AbstractRdbmsStore.java
@@ -86,7 +86,9 @@ RdbmsStore {
 	private Statement unbatchedStatement = null;
 	public void addUnbatched(Atom a) {
 		try {
-			this.unbatchedStatement = this.createStatement();
+			if (this.unbatchedStatement == null) {
+				this.unbatchedStatement = this.createStatement();
+			}
 			this.add(this.unbatchedStatement, a);
 			if((++this.unbatchedAtoms % MAX_BATCH_SIZE) == 0) {
 				if(logger.isDebugEnabled()) {
