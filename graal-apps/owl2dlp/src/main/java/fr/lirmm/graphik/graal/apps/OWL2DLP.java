7726041da5bccacf74310d16a46b771e128f859b
Clément SIPIETER <clement@6pi.fr>
2015-01-06 14:48:44 +0100

    owl2dlp: mv printVersion method in graal-util

--- graal-apps/owl2dlp/src/main/java/fr/lirmm/graphik/graal/apps/OWL2DLP.java
+++ graal-apps/owl2dlp/src/main/java/fr/lirmm/graphik/graal/apps/OWL2DLP.java
@@ -5,10 +5,6 @@ package fr.lirmm.graphik.graal.apps;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.jar.Attributes;
-import java.util.jar.Manifest;
 
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
@@ -18,6 +14,7 @@ import com.beust.jcommander.Parameter;
 import fr.lirmm.graphik.graal.io.dlgp.DlgpWriter;
 import fr.lirmm.graphik.graal.io.owl.OWLParser;
 import fr.lirmm.graphik.graal.io.owl.OWLParserException;
+import fr.lirmm.graphik.util.Apps;
 
 /**
  * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
@@ -56,7 +53,7 @@ public class OWL2DLP {
 		}
 
 		if (options.version) {
-			printVersion();
+			Apps.printVersion("owl2dlp");
 			System.exit(0);
 		}
 
@@ -84,71 +81,5 @@ public class OWL2DLP {
 
 	}
 	
-	private static void printVersion() {
-		Manifest manifest;
-		InputStream is;
-		Attributes att;
-		URL pathToManifest;
-
-		String version;
-		String vendor;
-		String buildDate;
-
-		// GET DATA
-		try {
-			pathToManifest = new URL(getPathToManifest());
-			is = pathToManifest.openStream();
-			manifest = new Manifest(is);
-			att = manifest.getMainAttributes();
-
-			version = att.getValue("Specification-Version");
-			vendor = att.getValue("Specification-Vendor");
-			buildDate = att.getValue("Built-On");
-
-			is.close();
-		} catch (Exception ex) {
-			version = vendor = buildDate = "?";
-		}
-
-		// PRINT DATA
-		System.out.print("owl2dlp version \"");
-		System.out.print(version);
-		System.out.println("\"");
-		System.out.print("Built on ");
-		System.out.println(buildDate);
-		System.out.print("Produced by ");
-		System.out.println(vendor);
-	}
-
-	private static String getPathToManifest() {
-
-		// 1 - Lire le nom de la classe
-		String classSimpleName = OWL2DLP.class.getSimpleName() + ".class";
-		// classSimpleName = VersionUtil.class
-
-		// 2 - Récupérer le chemin physique de la classe
-		String pathToClass = OWL2DLP.class.getResource(classSimpleName)
-				.toString();
-		// pathToClass =
-		// file:/C:/workspace/VersionUtil/bin/com/abdennebi/version/VersionUtil.class
-		// pathToClass =
-		// jar:file:/C:/version.jar!/com/abdennebi/version/VersionUtil.class
-
-		// 3 - Récupérer le chemin de la classe à partir de la racine du
-		// classpath
-		String classFullName = OWL2DLP.class.getName().replace('.', '/')
-				+ ".class";
-		// classFullName = com/abdennebi/version/VersionUtil.class
-
-		// 4 - Récupérer le chemin complet vers MANIFEST.MF
-		String pathToManifest = pathToClass.substring(0, pathToClass.length()
-				- (classFullName.length()))
-				+ "META-INF/MANIFEST.MF";
-		// pathToManifest =
-		// file:/C:/workspace/VersionUtil/bin/META-INF/MANIFEST.MF
-		// pathToManifest = jar:file:/C:/version.jar!/META-INF/MANIFEST.MF
-
-		return pathToManifest;
-	}
 
 }
---
+++ graal-util/src/main/java/fr/lirmm/graphik/util/Apps.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package fr.lirmm.graphik.util;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+/**
+ * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
+ *
+ */
+public final class Apps {
+	
+	private Apps(){}
+	
+	public static void printVersion(String applicationName) {
+		Manifest manifest;
+		InputStream is;
+		Attributes att;
+		URL pathToManifest;
+
+		String version;
+		String vendor;
+		String buildDate;
+
+		// GET DATA
+		try {
+			pathToManifest = new URL(getPathToManifest());
+			is = pathToManifest.openStream();
+			manifest = new Manifest(is);
+			att = manifest.getMainAttributes();
+
+			version = att.getValue("Specification-Version");
+			vendor = att.getValue("Specification-Vendor");
+			buildDate = att.getValue("Built-On");
+
+			is.close();
+		} catch (Exception ex) {
+			version = vendor = buildDate = "?";
+		}
+
+		// PRINT DATA
+		System.out.print(applicationName);
+		System.out.print(" version \"");
+		System.out.print(version);
+		System.out.println("\"");
+		System.out.print("Built on ");
+		System.out.println(buildDate);
+		System.out.print("Produced by ");
+		System.out.println(vendor);
+	}
+
+	private static String getPathToManifest() {
+
+		// 1 - Lire le nom de la classe
+		String classSimpleName = Apps.class.getSimpleName() + ".class";
+		// classSimpleName = VersionUtil.class
+
+		// 2 - Récupérer le chemin physique de la classe
+		String pathToClass = Apps.class.getResource(classSimpleName)
+				.toString();
+		// pathToClass =
+		// file:/C:/workspace/VersionUtil/bin/com/abdennebi/version/VersionUtil.class
+		// pathToClass =
+		// jar:file:/C:/version.jar!/com/abdennebi/version/VersionUtil.class
+
+		// 3 - Récupérer le chemin de la classe à partir de la racine du
+		// classpath
+		String classFullName = Apps.class.getName().replace('.', '/')
+				+ ".class";
+		// classFullName = com/abdennebi/version/VersionUtil.class
+
+		// 4 - Récupérer le chemin complet vers MANIFEST.MF
+		String pathToManifest = pathToClass.substring(0, pathToClass.length()
+				- (classFullName.length()))
+				+ "META-INF/MANIFEST.MF";
+		// pathToManifest =
+		// file:/C:/workspace/VersionUtil/bin/META-INF/MANIFEST.MF
+		// pathToManifest = jar:file:/C:/version.jar!/META-INF/MANIFEST.MF
+
+		return pathToManifest;
+	}
+
+
+}
