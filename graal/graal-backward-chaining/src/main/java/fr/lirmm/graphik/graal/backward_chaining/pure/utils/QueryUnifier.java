b0ebc3d98f1e3d0081a0ac67fb1ed2d09fa77860
Clément SIPIETER <clement@6pi.fr>
2015-06-09 15:59:14 +0200

    Add a license header

--- graal/graal-backward-chaining/src/main/java/fr/lirmm/graphik/graal/backward_chaining/pure/utils/QueryUnifier.java
+++ graal/graal-backward-chaining/src/main/java/fr/lirmm/graphik/graal/backward_chaining/pure/utils/QueryUnifier.java
@@ -1,4 +1,16 @@
-package fr.lirmm.graphik.graal.backward_chaining.pure.utils;
+/* Graal v0.7.4
+ * Copyright (c) 2014-2015 Inria Sophia Antipolis - Méditerranée / LIRMM (Université de Montpellier & CNRS)
+ * All rights reserved.
+ * This file is part of Graal <https://graphik-team.github.io/graal/>.
+ *
+ * Author(s): Clément SIPIETER
+ *            Mélanie KÖNIG
+ *            Swan ROCHER
+ *            Jean-François BAGET
+ *            Michel LECLÈRE
+ *            Marie-Laure MUGNIER
+ */
+ package fr.lirmm.graphik.graal.backward_chaining.pure.utils;
 
 import fr.lirmm.graphik.graal.core.Atom;
 import fr.lirmm.graphik.graal.core.ConjunctiveQuery;
