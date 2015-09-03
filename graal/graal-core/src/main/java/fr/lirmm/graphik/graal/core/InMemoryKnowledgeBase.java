f6a8f70e33b393340d2944623397cd21a522e7db
Clément SIPIETER <clement@6pi.fr>
2015-09-03 10:29:57 +0200

    InMemoryKnownledgeBase

--- graal/graal-core/src/main/java/fr/lirmm/graphik/graal/core/InMemoryKnowledgeBase.java
+++ graal/graal-core/src/main/java/fr/lirmm/graphik/graal/core/InMemoryKnowledgeBase.java
@@ -40,76 +40,17 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
- /**
- * 
- */
-package fr.lirmm.graphik.graal.core.impl;
+package fr.lirmm.graphik.graal.core;
 
-import java.util.Iterator;
-
-import fr.lirmm.graphik.graal.core.Atom;
-import fr.lirmm.graphik.graal.core.InMemoryKnowledgeBase;
-import fr.lirmm.graphik.graal.core.Rule;
-import fr.lirmm.graphik.graal.core.atomset.AtomSetException;
 import fr.lirmm.graphik.graal.core.atomset.InMemoryAtomSet;
-import fr.lirmm.graphik.graal.core.factory.AtomSetFactory;
-import fr.lirmm.graphik.graal.core.ruleset.LinkedListRuleSet;
-import fr.lirmm.graphik.graal.core.ruleset.RuleSet;
 
 /**
  * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
- * 
+ *
  */
-public class DefaultKnowledgeBase implements InMemoryKnowledgeBase {
-
-	private RuleSet ruleset;
-	private InMemoryAtomSet atomset;
-
-	// /////////////////////////////////////////////////////////////////////////
-	// CONSTRUCTOR
-	// /////////////////////////////////////////////////////////////////////////
-
-	public DefaultKnowledgeBase() {
-		this.ruleset = new LinkedListRuleSet();
-		this.atomset = AtomSetFactory.instance().createAtomSet();
-	}
-
-	public DefaultKnowledgeBase(RuleSet ontology, InMemoryAtomSet facts) {
-		this.ruleset = ontology;
-		this.atomset = facts;
-	}
-
-	// /////////////////////////////////////////////////////////////////////////
-	// GETTERS/SETTERS
-	// /////////////////////////////////////////////////////////////////////////
-
-	/**
-	 * @return the ruleset
-	 */
-	@Override
-	public RuleSet getOntology() {
-		return ruleset;
-	}
-
-	/**
-	 * @return the atomset
-	 */
-	@Override
-	public InMemoryAtomSet getFacts() {
-		return atomset;
-	}
+public interface InMemoryKnowledgeBase extends KnowledgeBase {
 
 	@Override
-	public void load(Iterator<Object> parser) throws AtomSetException {
-		Object o;
-		while (parser.hasNext()) {
-			o = parser.next();
-			if (o instanceof Rule) {
-				this.getOntology().add((Rule) o);
-			} else if (o instanceof Atom) {
-				this.getFacts().add((Atom) o);
-			}
-		}
-	}
+	InMemoryAtomSet getFacts();
 
-};
+}
