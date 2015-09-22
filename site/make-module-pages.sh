a919192651a80ac3afd1d007eb02f1cb55642124
Clément SIPIETER <clement@6pi.fr>
2015-09-22 16:50:17 +0200

    use template toolkit instead of preproc C

--- site/make-module-pages.sh
+++ site/make-module-pages.sh
@@ -4,25 +4,22 @@ module=$(basename $1)
 title=$module
 deplist=$(mvn dependency:list | grep ":compile" | sort | uniq | sed 's/\[INFO\]\s*\([^:]*\):\([^:]*\):jar:\([^:]*\):compile.*/\1:\2:\3/' |  perl -n -e 'my $t; if (/(.*?):(.*)/) {$t = $2;} $_ = $1; s/[.:]/\//g; print "$_/$t\n"' | perl -n -e '/(.*)\/(.*)\/(.*)/ && print "/$1/$2/$3/$3.jar\n"' | sed 's@:@/@' | sed 's@:@-@')
 
-
 echo '
 <!DOCTYPE html>
-#include "../config.h"
 <html>
-  
-  #define __BASEDIR__ ../
-  #define __TITLE__ '$title'
-  #include "../header.html"
+  [%- PROCESS config.tt -%]
+  [%- title = "'$title'" 
+      config.basedir="../"
+  -%]  
+  [%- INCLUDE header.tt -%]
   
   <body>
 
     <!-- HEADER -->
     <div id="header_wrap" class="outer">
         <header class="inner">
-
-          <h1 id="project_title">'$title'</h1>
-          #include "../nav.html"
-
+          <h1 id="project_title">[% title %]</h1>
+          [%- INCLUDE nav.tt -%]
         </header>
     </div>
 
@@ -31,13 +28,13 @@ echo '
       <section id="main_content" class="inner">
 
       <h2>Download</h2>
-      <a href=STR(CONCAT5(../,__GRAAL_VERSION__,/'$module'-,__GRAAL_VERSION__,.jar))>'$module'-__GRAAL_VERSION__.jar</a>
+      <a href="[% "../" _ config.graalVersion _ "/'$module'-" _ config.graalVersion _ ".jar" %]">'$module'-[% config.graalVersion %].jar</a>
 
       <h2>Maven</h2>
 <pre><code>&lt;dependency&gt;
     &lt;groupId&gt;fr.lirmm.graphik&lt;/groupId&gt;
     &lt;artifactId&gt;'$module'&lt;/artifactId&gt;
-    &lt;version&gt;'__GRAAL_VERSION__'&lt;/version&gt;
+    &lt;version&gt;[% config.graalVersion %]&lt;/version&gt;
 &lt;/dependency&gt;
 </code></pre>
       <h2>Dependency list</h2>
@@ -51,7 +48,7 @@ echo '        </ul>
       </section>
     </div>
 
-    #include "../footer.html"
+  [%- INCLUDE footer.tt -%]
 
   </body>
 </html>'
