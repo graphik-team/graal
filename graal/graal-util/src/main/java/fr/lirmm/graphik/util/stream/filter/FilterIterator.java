d7407ee7d52972be9a502c3678c6c2ca8e7ac7a5
Clément SIPIETER <clement@6pi.fr>
2016-01-25 15:01:54 +0100

    Graal-util: fix possible StackOverflowException

--- graal/graal-util/src/main/java/fr/lirmm/graphik/util/stream/filter/FilterIterator.java
+++ graal/graal-util/src/main/java/fr/lirmm/graphik/util/stream/filter/FilterIterator.java
@@ -67,12 +67,10 @@ public class FilterIterator<U, T> extends AbstractIterator<T> {
 
 	@Override
 	public boolean hasNext() {
-		if(this.next == null && this.it.hasNext()) {
+		while (this.next == null && this.it.hasNext()) {
 			U o = this.it.next();
 			if(this.filter.filter(o)) {
 				this.next = (T) o;
-			} else {
-				this.hasNext();
 			}
 		}
 		return this.next != null;
