3acc7df708949c5c5e4fc8b2a7c463662a48047d
Swan Rocher <swan.rocher@lirmm.fr>
2016-12-15 11:41:33 +0100

    Fix dep script

--- scripts/dep-list-create.sh
+++ scripts/dep-list-create.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn dependency:list | grep ":compile" | grep -v graal | sort | uniq | sed 's/\[INFO\]\s*\([^ ]*\):compile.*/\1/' | sed 's/:jar//' | perl -n -e 'my $t; if (/(.*?):(.*)/) {$t = $2;} $_ = $1; s/[.:]/\//g; print "$_/$t\n"' | perl -n -e '/(.*)\/(.*)\/(.*)/ && print "/$1/$2/$3/$3.jar\n"' | sed 's-:-/-' | sed 's.:.-.'
+mvn dependency:list | grep ":compile" | grep -v graal | grep -v SNAPSHOT | sort | uniq | sed 's/\[INFO\]\s*\([^ ]*\):compile.*/\1/' | sed 's/:jar//' | perl -n -e 'my $t; if (/(.*?):(.*)/) {$t = $2;} $_ = $1; s/[.:]/\//g; print "$_/$t\n"' | perl -n -e '/(.*)\/(.*)\/(.*)/ && print "/$1/$2/$3/$3.jar\n"' | sed 's-:-/-' | sed 's.:.-.'
