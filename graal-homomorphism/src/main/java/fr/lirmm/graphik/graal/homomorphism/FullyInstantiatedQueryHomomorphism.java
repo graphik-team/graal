a76e0257f4910c603963639d49005515aa04fed4
Clément SIPIETER <clement@6pi.fr>
2017-01-26 14:24:52 +0100

    FullyInstantiatedQueryHomomorphismChecker: new homomorphism implementation dedicated to query without variables

---
+++ graal-homomorphism/src/main/java/fr/lirmm/graphik/graal/homomorphism/FullyInstantiatedQueryHomomorphism.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) Inria Sophia Antipolis - Méditerranée / LIRMM
+ * (Université de Montpellier & CNRS) (2014 - 2017)
+ *
+ * Contributors :
+ *
+ * Clément SIPIETER <clement.sipieter@inria.fr>
+ * Mélanie KÖNIG
+ * Swan ROCHER
+ * Jean-François BAGET
+ * Michel LECLÈRE
+ * Marie-Laure MUGNIER <mugnier@lirmm.fr>
+ *
+ *
+ * This file is part of Graal <https://graphik-team.github.io/graal/>.
+ *
+ * This software is governed by the CeCILL  license under French law and
+ * abiding by the rules of distribution of free software.  You can  use,
+ * modify and/ or redistribute the software under the terms of the CeCILL
+ * license as circulated by CEA, CNRS and INRIA at the following URL
+ * "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL license and that you accept its terms.
+ */
+package fr.lirmm.graphik.graal.homomorphism;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import fr.lirmm.graphik.graal.api.core.Atom;
+import fr.lirmm.graphik.graal.api.core.AtomSet;
+import fr.lirmm.graphik.graal.api.core.AtomSetException;
+import fr.lirmm.graphik.graal.api.core.ConjunctiveQuery;
+import fr.lirmm.graphik.graal.api.core.InMemoryAtomSet;
+import fr.lirmm.graphik.graal.api.core.RulesCompilation;
+import fr.lirmm.graphik.graal.api.core.Substitution;
+import fr.lirmm.graphik.graal.api.homomorphism.HomomorphismException;
+import fr.lirmm.graphik.graal.core.Substitutions;
+import fr.lirmm.graphik.util.stream.CloseableIterator;
+import fr.lirmm.graphik.util.stream.CloseableIteratorWithoutException;
+import fr.lirmm.graphik.util.stream.Iterators;
+
+/**
+ * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
+ *
+ */
+public class FullyInstantiatedQueryHomomorphism extends AbstractHomomorphismWithCompilation<ConjunctiveQuery, AtomSet> {
+
+	// /////////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS
+	// /////////////////////////////////////////////////////////////////////////
+
+	private static final FullyInstantiatedQueryHomomorphism INSTANCE = new FullyInstantiatedQueryHomomorphism();
+
+	private FullyInstantiatedQueryHomomorphism() {
+		
+	}
+	// /////////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS
+	// /////////////////////////////////////////////////////////////////////////
+
+	public static FullyInstantiatedQueryHomomorphism instance() {
+		return INSTANCE;
+	}
+
+	// /////////////////////////////////////////////////////////////////////////
+	// HOMOMORPHISM METHODS
+	// /////////////////////////////////////////////////////////////////////////
+	
+	
+	public <U2 extends AtomSet> CloseableIterator<Substitution> execute(InMemoryAtomSet q, U2 a) throws HomomorphismException {
+		try {
+			CloseableIteratorWithoutException<Atom> it = q.iterator();
+			boolean contains = true;
+			while(contains && it.hasNext()) {
+				contains = a.contains(it.next());
+			}
+			if(contains) {
+				return Iterators.singletonIterator(Substitutions.emptySubstitution());
+			} else {
+				return Iterators.emptyIterator();
+			}
+		} catch (AtomSetException e) {
+			throw new HomomorphismException(e);
+		}
+	}
+	
+	@Override
+	public <U1 extends ConjunctiveQuery, U2 extends AtomSet> CloseableIterator<Substitution> execute(U1 q, U2 a) throws HomomorphismException {
+		try {
+			CloseableIteratorWithoutException<Atom> it = q.getAtomSet().iterator();
+			boolean contains = true;
+			while(contains && it.hasNext()) {
+				contains = a.contains(it.next());
+			}
+			if(contains) {
+				return Iterators.singletonIterator(Substitutions.emptySubstitution());
+			} else {
+				return Iterators.emptyIterator();
+			}
+		} catch (AtomSetException e) {
+			throw new HomomorphismException(e);
+		}
+	}
+	
+	@Override
+	public <U1 extends ConjunctiveQuery, U2 extends AtomSet> CloseableIterator<Substitution> execute(U1 q, U2 a, RulesCompilation rc) throws HomomorphismException {
+		try {
+			CloseableIteratorWithoutException<Atom> it = q.getAtomSet().iterator();
+			boolean contains = true;
+			while(contains && it.hasNext()) {
+				Atom atom = it.next();
+				boolean containsAtom = false;
+				for(Pair<Atom, Substitution> im : rc.getRewritingOf(atom)) {
+					containsAtom = a.contains(im.getLeft());
+					if(containsAtom) {
+						break;
+					}
+				}
+				contains = containsAtom;
+			}
+			if(contains) {
+				return Iterators.singletonIterator(Substitutions.emptySubstitution());
+			} else {
+				return Iterators.emptyIterator();
+			}
+		} catch (AtomSetException e) {
+			throw new HomomorphismException(e);
+		}
+	}
+	
+}
