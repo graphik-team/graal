95f5caf3857bd643c0638119e910d2cbb26ed5e1
Clément SIPIETER <clement@6pi.fr>
2017-01-26 14:36:19 +0100

    Utils: add a Trie structure

---
+++ graal-util/src/main/java/fr/lirmm/graphik/util/collections/Trie.java
@@ -0,0 +1,37 @@
+package fr.lirmm.graphik.util.collections;
+
+public class Trie<T,V>
+{
+   private TrieNode<T,V> root;
+   
+   /**
+    * Constructor
+    */
+   public Trie()
+   {
+      root = new TrieNode<T,V>();
+   }
+   
+   /**
+    * Associates a value to a word
+    * 
+    * @param value
+    * @param word
+    */
+   public V put(V value, T... word)
+   {
+      return root.put(value, 0, word);
+   }
+   
+   /**
+    * Get the value in the Trie with the given
+    * word
+    * 
+    * @param word
+    * @return the associated value, or null if there is no value associated.
+    */
+   public V get(T... word)
+   {
+      return root.get(0, word);
+   }
+}
---
+++ graal-util/src/main/java/fr/lirmm/graphik/util/collections/TrieNode.java
@@ -0,0 +1,73 @@
+package fr.lirmm.graphik.util.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TrieNode<T, V> {
+	
+	private Map<T,TrieNode<T,V>> children;
+	private boolean isLeaf; // quick way to check if any children exist
+	private V value; // the associated value to this word, if exist.
+
+	// /////////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS
+	// /////////////////////////////////////////////////////////////////////////
+	
+	public TrieNode() {
+		children = new HashMap<T,TrieNode<T,V>>();
+		isLeaf = true;
+	}
+	
+	// /////////////////////////////////////////////////////////////////////////
+	// PUBLIC METHODS
+	// /////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Adds a word to this node. This method is called recursively and adds
+	 * child nodes for each successive letter in the word, therefore recursive
+	 * calls will be made with partial words.
+	 * 
+	 * @param word
+	 *            the word to add
+	 */
+	public V put(V value, int index, T... word) {
+		if (word.length <= index) {
+			V tmp = this.value;
+			this.value = value;
+			return tmp;
+		} else {
+			isLeaf = false;
+			T charac = word[index];
+			TrieNode<T,V> child = children.get(charac);
+			if (child == null) {
+				child = new TrieNode<T,V>();
+				children.put(charac, child);
+			}
+			return child.put(value, index + 1, word);
+		}
+	}
+
+	/**
+	 * 
+	 * @return the associated value to the specified word.
+	 */
+	public V get(int index, T... word) {
+		if (word.length <= index) {
+			return this.value;
+		} else {
+			if(isLeaf) {
+				return null;
+			} else {
+				T charac = word[index];
+				TrieNode<T,V> child = children.get(charac);
+				if(child == null) {
+					return null;
+				} else {
+					return child.get(index + 1, word);
+				}
+			}
+		}
+	}
+
+
+}
