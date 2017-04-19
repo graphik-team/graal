graphik-team/graal

commit 04ee440abd6526b36c6ea6289589cd8934fa96b6

Create an interface for GraphOfRuleDependencies

graal-core/src/main/java/fr/lirmm/graphik/graal/core/grd/DefaultGraphOfRuleDependencies.java
@@ -43,11 +43,12 @@
  /**
  * 
  */
-package fr.lirmm.graphik.graal.grd;
+package fr.lirmm.graphik.graal.core.grd;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -56,21 +57,24 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.jgrapht.DirectedGraph;
 import org.jgrapht.alg.CycleDetector;
 import org.jgrapht.graph.DefaultDirectedGraph;
 
 import fr.lirmm.graphik.graal.api.core.Atom;
+import fr.lirmm.graphik.graal.api.core.GraphOfRuleDependencies;
 import fr.lirmm.graphik.graal.api.core.Predicate;
 import fr.lirmm.graphik.graal.api.core.Rule;
 import fr.lirmm.graphik.graal.api.core.Substitution;
 import fr.lirmm.graphik.graal.core.LabelRuleComparator;
+import fr.lirmm.graphik.graal.core.Substitutions;
 import fr.lirmm.graphik.graal.core.TreeMapSubstitution;
 import fr.lirmm.graphik.graal.core.Unifier;
 import fr.lirmm.graphik.util.LinkedSet;
 import fr.lirmm.graphik.util.graph.scc.StronglyConnectedComponentsGraph;
 import fr.lirmm.graphik.util.stream.CloseableIteratorWithoutException;
-import fr.lirmm.graphik.util.stream.filter.Filter;
 
 /**
  * The graph of rule dependencies (GRD) is a directed graph built from a rule
@@ -84,47 +88,22 @@ import fr.lirmm.graphik.util.stream.filter.Filter;
  *         Swan Rocher {@literal <swan.rocher@lirmm.fr>}
  * 
  */
-public class GraphOfRuleDependencies {
-
-	public static abstract class DependencyChecker implements Filter<Substitution> {
-		@Override
-		public final boolean filter(Substitution s) {
-			return isValidDependency(this.rule1,this.rule2,s);
-		}
-
-		protected abstract boolean isValidDependency(Rule r1, Rule r2, Substitution s);
-
-		public final void setRule1(Rule r1) { 
-			Substitution s = Unifier.computeInitialSourceTermsSubstitution(r1);
-			this.rule1 = s.createImageOf(r1); 
-		}
-		public final void setRule2(Rule r2) { 
-			Substitution s = Unifier.computeInitialTargetTermsSubstitution(r2.getBody());
-			this.rule2 = s.createImageOf(r2); 
-		}
-
-		private Rule rule1;
-		private Rule rule2;
-
-		public static final DependencyChecker DEFAULT = new DependencyChecker() {
-			@Override
-			protected boolean isValidDependency(Rule r1, Rule r2, Substitution s) { return true; }
-		};
-	};
+public class DefaultGraphOfRuleDependencies implements GraphOfRuleDependencies {
 
 	private DirectedGraph<Rule, Integer> graph;
 	private ArrayList<Set<Substitution>> edgesValue;
+	private DependencyChecker checker;
 	private boolean computingUnifiers;
 
 	// /////////////////////////////////////////////////////////////////////////
 	// CONSTRUCTORS
 	// /////////////////////////////////////////////////////////////////////////
 
-	public GraphOfRuleDependencies(Iterable<Rule> rules, boolean withUnifiers, DependencyChecker checker) {
+	public DefaultGraphOfRuleDependencies(Iterable<Rule> rules, boolean withUnifiers, DependencyChecker checker) {
 		this(rules.iterator(), withUnifiers, checker);
 	}
 	
-	public GraphOfRuleDependencies(Iterator<Rule> rules, boolean withUnifiers, DependencyChecker checker) {
+	public DefaultGraphOfRuleDependencies(Iterator<Rule> rules, boolean withUnifiers, DependencyChecker checker) {
 		this.graph = new DefaultDirectedGraph<Rule, Integer>(Integer.class);
 		this.edgesValue = new ArrayList<Set<Substitution>>();
 		this.computingUnifiers = withUnifiers;
@@ -133,62 +112,57 @@ public class GraphOfRuleDependencies {
 			this.addRule(rules.next());
 		}
 
+		this.checker = checker;
 		this.computeDependencies(checker);
 	}
 
-	public GraphOfRuleDependencies(Iterable<Rule> rules, boolean wu) {
+	public DefaultGraphOfRuleDependencies(Iterable<Rule> rules, boolean wu) {
 		this(rules.iterator(),wu);
 	}
 	
-	public GraphOfRuleDependencies(Iterator<Rule> rules, boolean wu) {
+	public DefaultGraphOfRuleDependencies(Iterator<Rule> rules, boolean wu) {
 		this(rules,wu,DependencyChecker.DEFAULT);
 	}
 
-	public GraphOfRuleDependencies(Iterable<Rule> rules, DependencyChecker checker) {
+	public DefaultGraphOfRuleDependencies(Iterable<Rule> rules, DependencyChecker checker) {
 		this(rules.iterator(),false,checker);
 	}
 
-	public GraphOfRuleDependencies(Iterable<Rule> rules) {
+	public DefaultGraphOfRuleDependencies(Iterable<Rule> rules) {
 		this(rules,false);
 	}
 	
-	public GraphOfRuleDependencies(Iterator<Rule> rules) {
+	public DefaultGraphOfRuleDependencies(Iterator<Rule> rules) {
 		this(rules,false);
 	}
 
-	protected GraphOfRuleDependencies(boolean wu) {
+	protected DefaultGraphOfRuleDependencies(boolean wu) {
 		this.graph = new DefaultDirectedGraph<Rule, Integer>(Integer.class);
 		this.edgesValue = new ArrayList<Set<Substitution>>();
 		this.computingUnifiers = wu;
 	}
 
-	protected GraphOfRuleDependencies() {
-		this.graph = new DefaultDirectedGraph<Rule, Integer>(Integer.class);
-		this.edgesValue = new ArrayList<Set<Substitution>>();
-		this.computingUnifiers = false;
-	}
-
-
-
 	// /////////////////////////////////////////////////////////////////////////
 	// PUBLIC METHODS
 	// /////////////////////////////////////////////////////////////////////////
 
+	@Override
 	public boolean hasCircuit() {
 		CycleDetector<Rule,Integer> cycle = new CycleDetector<Rule,Integer>(this.graph);
 		return cycle.detectCycles();
 	}
 
 	public Set<Substitution> getUnifiers(Integer e) {
-		return Collections.unmodifiableSet(this.edgesValue.get(e));
+		if(this.computingUnifiers) {
+			return Collections.unmodifiableSet(this.edgesValue.get(e));
+		} else {
+			return this.computeDependency(this.graph.getEdgeSource(e), this.graph.getEdgeTarget(e), this.checker);
+		}
 	}
 
-	/**
-	 * @param ruleSet
-	 * @return a GraphOfRuleDependencies which is a subgraph of this graph containing only specified rules.
-	 */
-	public GraphOfRuleDependencies getSubGraph(Iterable<Rule> ruleSet) {
-		GraphOfRuleDependencies subGRD = new GraphOfRuleDependencies();
+	@Override
+	public DefaultGraphOfRuleDependencies getSubGraph(Iterable<Rule> ruleSet) {
+		DefaultGraphOfRuleDependencies subGRD = new DefaultGraphOfRuleDependencies(this.computingUnifiers);
 		subGRD.addRuleSet(ruleSet);
 		for (Rule src : ruleSet) {
 			for (Rule target : ruleSet) {
@@ -201,6 +175,62 @@ public class GraphOfRuleDependencies {
 			}
 		}
 		return subGRD;
+	}
+
+	@Override
+	public Iterable<Rule> getRules() {
+		return this.graph.vertexSet();
+	}
+
+	public Iterable<Integer> getOutgoingEdgesOf(Rule src) {
+		return this.graph.outgoingEdgesOf(src);
+	}
+	
+	@Override
+	public Set<Rule> getTriggeredRules(Rule src) {
+		Set<Rule> set = new HashSet<Rule>();
+		for(Integer i : this.graph.outgoingEdgesOf(src)) {
+			set.add(this.graph.getEdgeTarget(i));
+		}
+		return set;
+	}
+	
+	public Set<Pair<Rule,Substitution>> getTriggeredRulesWithUnifiers(Rule src) {
+		Set<Pair<Rule,Substitution>> set = new HashSet<Pair<Rule,Substitution>>();
+		for(Integer i : this.graph.outgoingEdgesOf(src)) {
+			Rule target = this.graph.getEdgeTarget(i);
+			for (Substitution u : this.getUnifiers(i)) {
+				Pair<Rule, Substitution> p = new ImmutablePair<Rule, Substitution>(target, u);
+				set.add(p);
+			}
+		}
+		return set;
+	}
+
+
+	public Rule getEdgeTarget(Integer i) {
+		return this.graph.getEdgeTarget(i);
+	}
+
+	@Override
+	public boolean existUnifier(Rule src, Rule dest) {
+		Integer index = this.graph.getEdge(src, dest);
+		return index != null;
+	}
+	
+	@Override
+	public Set<Substitution> getUnifiers(Rule src, Rule dest) {
+		Integer index = this.graph.getEdge(src, dest);
+		if(index != null) {
+			return this.getUnifiers(index);
+		} else {
+			return Collections.<Substitution>emptySet();
+		}
+	}
+
+	@Override
+	public StronglyConnectedComponentsGraph<Rule> getStronglyConnectedComponentsGraph() {
+		return new<Integer> StronglyConnectedComponentsGraph<Rule>(this.graph);
 	}
 
 	// /////////////////////////////////////////////////////////////////////////
@@ -250,10 +280,6 @@ public class GraphOfRuleDependencies {
 		edge.add(sub);
 	}
 
-	protected void addDependency(Rule src, Rule dest) {
-		addDependency(src,new TreeMapSubstitution(),dest);
-	}
-
 	protected void setDependency(Rule src, Set<Substitution> subs, Rule dest) {
 		Integer edgeIndex = this.graph.getEdge(src, dest);
 		if (edgeIndex == null) {
@@ -264,73 +290,15 @@ public class GraphOfRuleDependencies {
 			this.edgesValue.set(edgeIndex, subs);
 		}
 	}
-
-
-	// /////////////////////////////////////////////////////////////////////////
-	// PUBLIC METHODS
-	// /////////////////////////////////////////////////////////////////////////
-
-	public Iterable<Rule> getRules() {
-		return this.graph.vertexSet();
-	}
-
-	public Iterable<Integer> getOutgoingEdgesOf(Rule src) {
-		return this.graph.outgoingEdgesOf(src);
-	}
-
-	public Rule getEdgeTarget(Integer i) {
-		return this.graph.getEdgeTarget(i);
-	}
-
-	public boolean existUnifier(Rule src, Rule dest) {
-		Integer index = this.graph.getEdge(src, dest);
-		if (index != null) {
-			return true;
-		}
-		return false;
-	}
-
-	public StronglyConnectedComponentsGraph<Rule> getStronglyConnectedComponentsGraph() {
-		return new<Integer> StronglyConnectedComponentsGraph<Rule>(this.graph);
-	}
-
-
-	// /////////////////////////////////////////////////////////////////////////
-	// OVERRIDE METHODS
-
-//	@Override
-//	public String toString() {
-//		StringBuilder s = new StringBuilder();
-//		for (Rule src : this.graph.vertexSet()) {
-//			for (Integer e : this.graph.outgoingEdgesOf(src)) {
-//
-//				s.append(this.graph.getEdgeSource(e).getLabel());
-//				s.append("-->");
-//				s.append(this.graph.getEdgeTarget(e).getLabel());
-//				s.append('\n');
-//			}
-//
-//		}
-//		return s.toString();
-//	}
-
-	// /////////////////////////////////////////////////////////////////////////
-	// PROTECTED METHODS
-	// /////////////////////////////////////////////////////////////////////////
-
-	protected void computeDependency(Rule r1, Rule r2, DependencyChecker checker) {
+	
+	protected Set<Substitution> computeDependency(Rule r1, Rule r2, DependencyChecker checker) {
 		checker.setRule1(r1);
 		checker.setRule2(r2);
 		if (this.computingUnifiers) {
-			Set<Substitution> unifiers = Unifier.instance().computePieceUnifier(r1,r2.getBody(),checker);
-			if (!unifiers.isEmpty()) {
-				this.setDependency(r1, unifiers, r2);
-			}
+			return Unifier.instance().computePieceUnifier(r1,r2.getBody(),checker);
 		}
 		else {
-			if(Unifier.instance().existPieceUnifier(r1,r2.getBody(),checker)) {
-				this.addDependency(r1, r2);
-			}
+			return Collections.<Substitution>singleton(Substitutions.emptySubstitution());
 		}
 	}
 
@@ -373,8 +341,12 @@ public class GraphOfRuleDependencies {
 				candidates = index.get(a.getPredicate());
 				if (candidates != null) {
 					for (Rule r2 : candidates) {
-						if (marked.add(r2.getLabel()))
-							computeDependency(r1,r2,checker);
+						if (marked.add(r2.getLabel())) {
+							Set<Substitution> unifiers = computeDependency(r1,r2,checker);
+							if (!unifiers.isEmpty()) {
+								this.setDependency(r1, unifiers, r2);
+							}
+						}
 					}
 				}
 			}

graal-core/src/test/java/fr/lirmm/graphik/graal/grd/GRDTest.java
@@ -53,6 +53,8 @@ import org.junit.Test;
 import fr.lirmm.graphik.graal.api.core.Rule;
 import fr.lirmm.graphik.graal.core.TestUtils;
 import fr.lirmm.graphik.graal.core.factory.DefaultRuleFactory;
+import fr.lirmm.graphik.graal.core.grd.DefaultGraphOfRuleDependencies;
+import fr.lirmm.graphik.graal.core.grd.ProductivityFilter;
 
 /**
  * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
@@ -67,7 +69,7 @@ public class GRDTest {
 		rules.add(DefaultRuleFactory.instance().create(TestUtils.rX, TestUtils.sX));
 		rules.add(DefaultRuleFactory.instance().create(TestUtils.rX, TestUtils.pXY));
 
-		GraphOfRuleDependencies grd = new GraphOfRuleDependencies(rules, true, new ProductivityFilter());
+		DefaultGraphOfRuleDependencies grd = new DefaultGraphOfRuleDependencies(rules, true, new ProductivityFilter());
 		Assert.assertFalse(grd.existUnifier(rules.get(0), rules.get(1)));
 		Assert.assertFalse(grd.existUnifier(rules.get(1), rules.get(0)));
 		Assert.assertTrue(grd.existUnifier(rules.get(0), rules.get(2)));

graal-api/src/main/java/fr/lirmm/graphik/graal/api/core/GraphOfRuleDependencies.java
@@ -40,52 +40,73 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
- /**
- * 
- */
-package fr.lirmm.graphik.graal.forward_chaining;
+package fr.lirmm.graphik.graal.api.core;
 
-import fr.lirmm.graphik.graal.api.core.AtomSet;
-import fr.lirmm.graphik.graal.api.core.Rule;
-import fr.lirmm.graphik.graal.api.forward_chaining.Chase;
-import fr.lirmm.graphik.graal.api.forward_chaining.ChaseException;
-import fr.lirmm.graphik.graal.api.forward_chaining.RuleApplier;
-import fr.lirmm.graphik.graal.core.grd.DefaultGraphOfRuleDependencies;
+import java.util.Set;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import fr.lirmm.graphik.util.graph.scc.StronglyConnectedComponentsGraph;
 
 /**
  * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
  *
  */
-public class StaticChase {
-	
-	public static void executeChase(AtomSet atomSet, Iterable<Rule> ruleSet)
-			throws ChaseException {
-		Chase chase = new SccChase(ruleSet.iterator(), atomSet);
-		chase.execute();
-	}
-	
-	public static void executeChase(AtomSet atomSet, Iterable<Rule> ruleSet, RuleApplier ruleApplier)
-			throws ChaseException {
-		Chase chase = new SccChase(ruleSet.iterator(), atomSet, ruleApplier);
-		chase.execute();
-	}
+public interface GraphOfRuleDependencies {
 
-	public static void executeOneStepChase(AtomSet atomSet,
-			Iterable<Rule> ruleSet) throws ChaseException {
-		Chase chase = new SccChase(ruleSet.iterator(), atomSet);
-		chase.next();
-	}
+	/**
+	 * Returns true, if there exists an unifier from src to dest, false otherwise.
+	 * @param src
+	 * @param dest
+	 * @return true iff there exists an unifier from src to dest.
+	 */
+	boolean existUnifier(Rule src, Rule dest);
 	
-	public static void executeChase(AtomSet atomSet, DefaultGraphOfRuleDependencies grd)
-			throws ChaseException {
-		Chase chase = new SccChase(grd, atomSet);
-		chase.execute();
-	}
+	/**
+	 * Returns a set of Substitution representing the set of unifiers from src to dest.
+	 * @param src 
+	 * @param dest
+	 * @return a set of Substitution representing the set of unifiers from src to dest.
+	 */
+	Set<Substitution> getUnifiers(Rule src, Rule dest);
+	
+	/**
+	 * Returns all rules that can be triggered by the specified one.
+	 * @param src a Rule.
+	 * @return all rules that can be triggered by the specified one.
+	 */
+	Set<Rule> getTriggeredRules(Rule src);
+	
+	/**
+	 * Returns all pair of rule and unifier that can be triggered by the specfied rule.
+	 * @param src
+	 * @return all pair of rule and unifier that can be triggered by the specfied rule.
+	 */
+	Set<Pair<Rule,Substitution>> getTriggeredRulesWithUnifiers(Rule src);
+	
+	/**
+     * Performs cycle detection on the {@link GraphOfRuleDependencies}.
+     *
+     * @return true iff the graph contains at least one cycle.
+     */
+	boolean hasCircuit();
 
-	public static void executeOneStepChase(AtomSet atomSet,
-			DefaultGraphOfRuleDependencies grd) throws ChaseException {
-		Chase chase = new SccChase(grd, atomSet);
-		chase.next();
-	}
+	/**
+	 * Creates and return an induced SubGraph of the current one.
+	 * @param ruleSet
+	 * @return the induced subgraph by specified rules.
+	 */
+	GraphOfRuleDependencies getSubGraph(Iterable<Rule> ruleSet);
+
+	/**
+	 * @return all rules containing in this {@link GraphOfRuleDependencies}.
+	 */
+	Iterable<Rule> getRules();
+
+
+	/**
+	 * @return the graph of strongly connected components of this graph.
+	 */
+	StronglyConnectedComponentsGraph<Rule> getStronglyConnectedComponentsGraph();
 
 }

graal-core/src/main/java/fr/lirmm/graphik/graal/core/grd/DependencyChecker.java
@@ -40,52 +40,35 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
- /**
- * 
- */
-package fr.lirmm.graphik.graal.forward_chaining;
+package fr.lirmm.graphik.graal.core.grd;
 
-import fr.lirmm.graphik.graal.api.core.AtomSet;
 import fr.lirmm.graphik.graal.api.core.Rule;
-import fr.lirmm.graphik.graal.api.forward_chaining.Chase;
-import fr.lirmm.graphik.graal.api.forward_chaining.ChaseException;
-import fr.lirmm.graphik.graal.api.forward_chaining.RuleApplier;
-import fr.lirmm.graphik.graal.core.grd.DefaultGraphOfRuleDependencies;
+import fr.lirmm.graphik.graal.api.core.Substitution;
+import fr.lirmm.graphik.graal.core.Unifier;
+import fr.lirmm.graphik.util.stream.filter.Filter;
 
-/**
- * @author Clément Sipieter (INRIA) {@literal <clement@6pi.fr>}
- *
- */
-public class StaticChase {
-	
-	public static void executeChase(AtomSet atomSet, Iterable<Rule> ruleSet)
-			throws ChaseException {
-		Chase chase = new SccChase(ruleSet.iterator(), atomSet);
-		chase.execute();
-	}
-	
-	public static void executeChase(AtomSet atomSet, Iterable<Rule> ruleSet, RuleApplier ruleApplier)
-			throws ChaseException {
-		Chase chase = new SccChase(ruleSet.iterator(), atomSet, ruleApplier);
-		chase.execute();
+public abstract class DependencyChecker implements Filter<Substitution> {
+	@Override
+	public final boolean filter(Substitution s) {
+		return isValidDependency(this.rule1,this.rule2,s);
 	}
 
-	public static void executeOneStepChase(AtomSet atomSet,
-			Iterable<Rule> ruleSet) throws ChaseException {
-		Chase chase = new SccChase(ruleSet.iterator(), atomSet);
-		chase.next();
+	protected abstract boolean isValidDependency(Rule r1, Rule r2, Substitution s);
+
+	public final void setRule1(Rule r1) { 
+		Substitution s = Unifier.computeInitialSourceTermsSubstitution(r1);
+		this.rule1 = s.createImageOf(r1); 
 	}
-	
-	public static void executeChase(AtomSet atomSet, DefaultGraphOfRuleDependencies grd)
-			throws ChaseException {
-		Chase chase = new SccChase(grd, atomSet);
-		chase.execute();
+	public final void setRule2(Rule r2) { 
+		Substitution s = Unifier.computeInitialTargetTermsSubstitution(r2.getBody());
+		this.rule2 = s.createImageOf(r2); 
 	}
 
-	public static void executeOneStepChase(AtomSet atomSet,
-			DefaultGraphOfRuleDependencies grd) throws ChaseException {
-		Chase chase = new SccChase(grd, atomSet);
-		chase.next();
-	}
+	private Rule rule1;
+	private Rule rule2;
 
+	public static final DependencyChecker DEFAULT = new DependencyChecker() {
+		@Override
+		protected boolean isValidDependency(Rule r1, Rule r2, Substitution s) { return true; }
+	};
 }

graal-core/src/main/java/fr/lirmm/graphik/graal/core/grd/ProductivityFilter.java
@@ -40,7 +40,7 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
- package fr.lirmm.graphik.graal.grd;
+ package fr.lirmm.graphik.graal.core.grd;
 
 import fr.lirmm.graphik.graal.api.core.InMemoryAtomSet;
 import fr.lirmm.graphik.graal.api.core.Rule;
@@ -48,7 +48,7 @@ import fr.lirmm.graphik.graal.api.core.Substitution;
 import fr.lirmm.graphik.graal.core.atomset.AtomSetUtils;
 import fr.lirmm.graphik.graal.core.atomset.LinkedListAtomSet;
 
-public class ProductivityFilter extends GraphOfRuleDependencies.DependencyChecker {
+public class ProductivityFilter extends DependencyChecker {
 
 	@Override
 	protected boolean isValidDependency(Rule r1, Rule r2, Substitution s) {

graal-forward-chaining/src/main/java/fr/lirmm/graphik/graal/forward_chaining/ChaseWithGRD.java
@@ -55,12 +55,13 @@ import org.slf4j.LoggerFactory;
 
 import fr.lirmm.graphik.graal.api.core.Atom;
 import fr.lirmm.graphik.graal.api.core.AtomSet;
+import fr.lirmm.graphik.graal.api.core.GraphOfRuleDependencies;
 import fr.lirmm.graphik.graal.api.core.Rule;
 import fr.lirmm.graphik.graal.api.forward_chaining.AbstractChase;
 import fr.lirmm.graphik.graal.api.forward_chaining.ChaseException;
 import fr.lirmm.graphik.graal.api.forward_chaining.RuleApplier;
+import fr.lirmm.graphik.graal.core.grd.DefaultGraphOfRuleDependencies;
 import fr.lirmm.graphik.graal.forward_chaining.rule_applier.RestrictedChaseRuleApplier;
-import fr.lirmm.graphik.graal.grd.GraphOfRuleDependencies;
 import fr.lirmm.graphik.util.stream.CloseableIteratorAdapter;
 
 /**
@@ -96,7 +97,7 @@ public class ChaseWithGRD extends AbstractChase {
 	}
 
 	public ChaseWithGRD(Iterator<Rule> rules, AtomSet atomSet) {
-		this(new GraphOfRuleDependencies(rules), atomSet);
+		this(new DefaultGraphOfRuleDependencies(rules), atomSet);
 	}
 
 	// /////////////////////////////////////////////////////////////////////////
@@ -116,8 +117,7 @@ public class ChaseWithGRD extends AbstractChase {
 				if (rule != null) {
 					boolean val = this.getRuleApplier().apply(rule, atomSet, newAtomSet);
 					if (val) {
-						for (Integer e : this.grd.getOutgoingEdgesOf(rule)) {
-							Rule triggeredRule = this.grd.getEdgeTarget(e);
+						for (Rule triggeredRule : this.grd.getTriggeredRules(rule)) {
 							if (LOGGER.isDebugEnabled()) {
 								LOGGER.debug("-- -- Dependency: " + triggeredRule);
 							}

graal-forward-chaining/src/main/java/fr/lirmm/graphik/graal/forward_chaining/ChaseWithGRDAndUnfiers.java
@@ -51,6 +51,7 @@ import java.util.List;
 import java.util.Queue;
 
 import org.apache.commons.lang3.tuple.ImmutableTriple;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,6 +59,7 @@ import org.slf4j.LoggerFactory;
 import fr.lirmm.graphik.graal.api.core.Atom;
 import fr.lirmm.graphik.graal.api.core.AtomSet;
 import fr.lirmm.graphik.graal.api.core.ConjunctiveQuery;
+import fr.lirmm.graphik.graal.api.core.GraphOfRuleDependencies;
 import fr.lirmm.graphik.graal.api.core.InMemoryAtomSet;
 import fr.lirmm.graphik.graal.api.core.Rule;
 import fr.lirmm.graphik.graal.api.core.Substitution;
@@ -77,13 +79,12 @@ import fr.lirmm.graphik.graal.core.atomset.LinkedListAtomSet;
 import fr.lirmm.graphik.graal.core.atomset.graph.DefaultInMemoryGraphStore;
 import fr.lirmm.graphik.graal.core.factory.DefaultConjunctiveQueryFactory;
 import fr.lirmm.graphik.graal.core.factory.DefaultSubstitutionFactory;
+import fr.lirmm.graphik.graal.core.grd.DefaultGraphOfRuleDependencies;
 import fr.lirmm.graphik.graal.core.term.DefaultTermFactory;
 import fr.lirmm.graphik.graal.forward_chaining.halting_condition.RestrictedChaseStopCondition;
 import fr.lirmm.graphik.graal.forward_chaining.rule_applier.DefaultRuleApplier;
-import fr.lirmm.graphik.graal.grd.GraphOfRuleDependencies;
 import fr.lirmm.graphik.graal.homomorphism.StaticHomomorphism;
 import fr.lirmm.graphik.util.stream.CloseableIterator;
-import fr.lirmm.graphik.util.stream.CloseableIteratorAdapter;
 import fr.lirmm.graphik.util.stream.IteratorException;
 import fr.lirmm.graphik.util.stream.Iterators;
 
@@ -119,7 +120,7 @@ public class ChaseWithGRDAndUnfiers extends AbstractChase {
 	}
 	
 	public ChaseWithGRDAndUnfiers(Iterator<Rule> rules, AtomSet atomSet) {
-		this(new GraphOfRuleDependencies(rules, true), atomSet, new DefaultRuleApplier());
+		this(new DefaultGraphOfRuleDependencies(rules, true), atomSet, new DefaultRuleApplier());
 	}
 	
 	// /////////////////////////////////////////////////////////////////////////
@@ -177,24 +178,22 @@ public class ChaseWithGRDAndUnfiers extends AbstractChase {
 									// Makes the projection compatible with triggered rules unifiers
 									Substitution compatibleProj = targetToSource(proj);
 									
-									for (Integer e : this.grd.getOutgoingEdgesOf(rule)) {
-										Rule triggeredRule = this.grd.getEdgeTarget(e);
+									for (Pair<Rule,Substitution> p : this.grd.getTriggeredRulesWithUnifiers(rule)) {
+										Rule triggeredRule = p.getKey();
+										Substitution u = p.getValue();
+										if(u != null) {
+											Substitution comp = unificator.compose(u);
+											Substitution aggreg = compatibleProj.aggregate(comp);
+											aggreg = forgetSource(aggreg);
+											if(LOGGER.isDebugEnabled()) {
+												LOGGER.debug("-- -- Dependency: {}", triggeredRule);
+												LOGGER.debug("-- -- Substitution:{} ", compatibleProj);
+												LOGGER.debug("-- -- Unificator: {}", u);
+												LOGGER.debug("-- -- Aggregation: {}\n", aggreg);
+											}
 									
-										for (Substitution u : this.grd.getUnifiers(e)) {
-											if(u != null) {
-												Substitution comp = unificator.compose(u);
-												Substitution aggreg = compatibleProj.aggregate(comp);
-												aggreg = forgetSource(aggreg);
-												if(LOGGER.isDebugEnabled()) {
-													LOGGER.debug("-- -- Dependency: {}", triggeredRule);
-													LOGGER.debug("-- -- Substitution:{} ", compatibleProj);
-													LOGGER.debug("-- -- Unificator: {}", u);
-													LOGGER.debug("-- -- Aggregation: {}\n", aggreg);
-												}
-										
-												if(aggreg != null) {
-													newQueue.add(new ImmutableTriple<Rule, Substitution, InMemoryAtomSet>(triggeredRule, aggreg, foundPart));
-												}
+											if(aggreg != null) {
+												newQueue.add(new ImmutableTriple<Rule, Substitution, InMemoryAtomSet>(triggeredRule, aggreg, foundPart));
 											}
 										}
 									}

commit b7c312538742009bf485776e82fd1d72c30a2a46

fix bug introduced in commit 04ee440abd6526b36c6ea6289589cd8934fa96b6

graal-core/src/main/java/fr/lirmm/graphik/graal/core/grd/DefaultGraphOfRuleDependencies.java
@@ -297,9 +297,10 @@ public class DefaultGraphOfRuleDependencies implements GraphOfRuleDependencies {
 		if (this.computingUnifiers) {
 			return Unifier.instance().computePieceUnifier(r1,r2.getBody(),checker);
 		}
-		else {
+		else if(Unifier.instance().existPieceUnifier(r1,r2.getBody(),checker)) {
 			return Collections.<Substitution>singleton(Substitutions.emptySubstitution());
 		}
+		return Collections.<Substitution>emptySet();
 	}
 
 

graal-core/src/test/java/fr/lirmm/graphik/graal/core/TestUtils.java
@@ -74,7 +74,7 @@ public final class TestUtils {
 	public static final Term A = DefaultTermFactory.instance().createConstant("a");
 	public static final Term B = DefaultTermFactory.instance().createConstant("b");
 
-	public static final Atom pTU, pUV, pVW, pWX, pXY, pYX, pYZ, pYW, pUU, pWV, pWT, pUW, pAU, pXA, pXB;
+	public static final Atom pTU, pUV, pVW, pWX, pXY, pYX, pYZ, pYW, pUU, pWV, pWT, pUW, pAU, pXA, pXB, pXX;
 	public static final Atom qXY, qTW, qUV, qVW;
 	public static final Atom sX, rX;
 
@@ -153,6 +153,11 @@ public final class TestUtils {
 		terms[0] = X;
 		terms[1] = B;
 		pXB = new DefaultAtom(p, Arrays.asList(terms));
+		
+		terms = new Term[2];
+		terms[0] = X;
+		terms[1] = X;
+		pXX = new DefaultAtom(p, Arrays.asList(terms));
 		
 		// q
 

graal-core/src/test/java/fr/lirmm/graphik/graal/core/grd/GRDTest.java
@@ -43,7 +43,7 @@
 /**
  * 
  */
-package fr.lirmm.graphik.graal.grd;
+package fr.lirmm.graphik.graal.core.grd;
 
 import java.util.LinkedList;
 
@@ -61,6 +61,29 @@ import fr.lirmm.graphik.graal.core.grd.ProductivityFilter;
  *
  */
 public class GRDTest {
+	
+	
+	@Test
+	public void simpleTest() {
+		LinkedList<Rule> rules = new LinkedList<Rule>();
+		rules.add(DefaultRuleFactory.instance().create(TestUtils.sX, TestUtils.rX));
+		rules.add(DefaultRuleFactory.instance().create(TestUtils.sX, TestUtils.pXY));
+
+		DefaultGraphOfRuleDependencies grd = new DefaultGraphOfRuleDependencies(rules, false);
+		Assert.assertFalse(grd.existUnifier(rules.get(0), rules.get(1)));
+		Assert.assertFalse(grd.existUnifier(rules.get(1), rules.get(0)));
+	}
+	
+	@Test
+	public void simpleTest2() {
+		LinkedList<Rule> rules = new LinkedList<Rule>();
+		rules.add(DefaultRuleFactory.instance().create(TestUtils.sX, TestUtils.pXY));
+		rules.add(DefaultRuleFactory.instance().create(TestUtils.pXX, TestUtils.rX));
+
+		DefaultGraphOfRuleDependencies grd = new DefaultGraphOfRuleDependencies(rules, false);
+		Assert.assertFalse(grd.existUnifier(rules.get(0), rules.get(1)));
+		Assert.assertFalse(grd.existUnifier(rules.get(1), rules.get(0)));
+	}
 
 	@Test
 	public void atomErasingFilterTest() {