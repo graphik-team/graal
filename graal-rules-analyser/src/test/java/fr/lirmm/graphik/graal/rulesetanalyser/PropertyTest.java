c92e4f0aa9233597c1727670fc572942b3df97ea
Clément SIPIETER <clement@6pi.fr>
2017-05-03 10:32:38 +0200

    RuleSetAnalyser: move unit test package

--- graal-rules-analyser/src/test/java/fr/lirmm/graphik/graal/rulesetanalyser/PropertyTest.java
+++ graal-rules-analyser/src/test/java/fr/lirmm/graphik/graal/rulesetanalyser/PropertyTest.java
@@ -40,6 +40,7 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
+package fr.lirmm.graphik.graal.rulesetanalyser;
 import static org.junit.Assert.assertEquals;
 
 import java.util.LinkedList;
@@ -58,12 +59,14 @@ import fr.lirmm.graphik.graal.rulesetanalyser.property.DomainRestrictedProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.FrontierGuardedProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.FrontierOneProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.GuardedProperty;
+import fr.lirmm.graphik.graal.rulesetanalyser.property.JointlyFrontierGuardedSetProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.LinearProperty;
+import fr.lirmm.graphik.graal.rulesetanalyser.property.MFAProperty;
+import fr.lirmm.graphik.graal.rulesetanalyser.property.MSAProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.RangeRestrictedProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.RuleSetProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.StickyProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.WeaklyAcyclicProperty;
-import fr.lirmm.graphik.graal.rulesetanalyser.property.JointlyFrontierGuardedSetProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.WeaklyGuardedSetProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.property.WeaklyStickyProperty;
 import fr.lirmm.graphik.graal.rulesetanalyser.util.AnalyserRuleSet;
@@ -90,11 +93,13 @@ public class PropertyTest {
 	private static RuleSetProperty ws = WeaklyStickyProperty.instance();
 	private static RuleSetProperty wg = WeaklyGuardedSetProperty.instance();
 	private static RuleSetProperty jfg = JointlyFrontierGuardedSetProperty.instance();
+	private static RuleSetProperty msa = MSAProperty.instance();
+	private static RuleSetProperty mfa = MFAProperty.instance();
 
 	private static LinkedList<RuleSetProperty> properties = new LinkedList<RuleSetProperty>();
 
 	private static Rule nr0,nr1,r0,r1,r2,r3,r4,r5;
-	private static List<Rule> rSet0, rSet1, rSet2, rSet3;
+	private static List<Rule> rSet0, rSet1, rSet2, rSet3, rSetMSA, rSetMFA, rSetSWA, rSetNotFes;
 	
 	@BeforeClass
 	public static void setUp() {
@@ -110,6 +115,8 @@ public class PropertyTest {
 		properties.add(ws);
 		properties.add(wg);
 		properties.add(jfg);
+		//properties.add(msa);
+		//properties.add(mfa);
 
 		try {
 
@@ -156,6 +163,34 @@ public class PropertyTest {
 		rSet3.add((Rule)(DlgpParser.parseRule("dept(W,Z) :- pR1(V,W,X,Y,Z).")));
 		rSet3.add((Rule)(DlgpParser.parseRule("runs(W,Y) :- pR1(V,W,X,Y,Z).")));
 		rSet3.add((Rule)(DlgpParser.parseRule("pro(Y,X):- runs(W,X),dept(W,Y).")));
+		
+		// Exemple 1 from "Acyclicity Notions for Existential Rules and
+		// Their Application to Query Answering in Ontologies" Grau, Horrocks, Krotzsch
+		rSetMSA = new LinkedList<Rule>(); // this set is not JA
+		rSetMSA.add(DlgpParser.parseRule("r(X1,Y1), b(Y1) :- a(X1)."));
+		rSetMSA.add(DlgpParser.parseRule("a(X2) :- r(X2,Z1), b(Z1)."));
+		rSetMSA.add(DlgpParser.parseRule("r(X3,Y2), c(Y2) :- b(X3)."));
+		rSetMSA.add(DlgpParser.parseRule("d(X4) :- c(X4)."));
+		rSetMSA.add(DlgpParser.parseRule("b(X5) :- r(X5,Z2), d(Z2)."));
+		
+		// Exemple 15 from "Acyclicity Notions for Existential Rules and
+		// Their Application to Query Answering in Ontologies" Grau, Horrocks, Krotzsch
+		rSetMFA = new LinkedList<Rule>(); // this set is not MSA
+		rSetMFA.add(DlgpParser.parseRule("r(X,Y), b(Y) :- a(X)."));
+		rSetMFA.add(DlgpParser.parseRule("s(X,Y), t(Y,X) :- b(X)."));
+		rSetMFA.add(DlgpParser.parseRule("c(X) :- a(Z), s(Z,X)."));
+		rSetMFA.add(DlgpParser.parseRule("a(X) :- c(Z), t(Z,X)."));
+		
+		// Exemple 20 from "Acyclicity Notions for Existential Rules and
+		// Their Application to Query Answering in Ontologies" Grau, Horrocks, Krotzsch
+		rSetSWA = new LinkedList<Rule>(); // this set is not JA
+		rSetSWA.add(DlgpParser.parseRule("r(X1,Y), r(Y,X1), r(X1,X1) :- a(X1)."));
+		rSetSWA.add(DlgpParser.parseRule("b(X2) :- r(X2,X2)."));
+		rSetSWA.add(DlgpParser.parseRule("a(X3) :- b(X3)."));
+				
+		rSetNotFes = new LinkedList<Rule>();
+		rSetNotFes.add(DlgpParser.parseRule("p(X,Y), h(Y) :- h(X)."));
+
 		}
 		catch (Exception e) {
 			e.printStackTrace();
@@ -359,6 +394,10 @@ public class PropertyTest {
 		assertEquals(-1, wa.check(new AnalyserRuleSet(rSet1)));
 		assertEquals(1, wa.check(new AnalyserRuleSet(rSet2)));
 		assertEquals(-1, wa.check(new AnalyserRuleSet(rSet3)));
+		assertEquals(-1, wa.check(new AnalyserRuleSet(rSetMSA)));
+		assertEquals(-1, wa.check(new AnalyserRuleSet(rSetMFA)));
+		assertEquals(-1, wa.check(new AnalyserRuleSet(rSetNotFes)));
+		assertEquals(-1, wa.check(new AnalyserRuleSet(rSetSWA)));
 	}
 
 	@Test
@@ -383,6 +422,29 @@ public class PropertyTest {
 		assertEquals(1, jfg.check(new AnalyserRuleSet(rSet1)));
 		assertEquals(1, jfg.check(new AnalyserRuleSet(rSet2)));
 		assertEquals(-1, jfg.check(new AnalyserRuleSet(rSet3)));
+	}
+	
+	@Test
+	public void setMSATest() {
+		assertEquals(1, msa.check(new AnalyserRuleSet(rSetMSA)));
+		assertEquals(-1, msa.check(new AnalyserRuleSet(rSetMFA)));
+		assertEquals(-1, msa.check(new AnalyserRuleSet(rSetNotFes)));
+		assertEquals(1, msa.check(new AnalyserRuleSet(rSetSWA)));
+	}
+	
+	@Test
+	public void setMFATest() {
+		assertEquals(1, mfa.check(new AnalyserRuleSet(rSetMSA)));
+		System.out.println("msa");
+		assertEquals(1, mfa.check(new AnalyserRuleSet(rSetMFA)));
+		System.out.println("mfa");
+
+		assertEquals(-1, mfa.check(new AnalyserRuleSet(rSetNotFes)));
+		System.out.println("notFes");
+
+		assertEquals(1, mfa.check(new AnalyserRuleSet(rSetSWA)));
+		System.out.println("SWA");
+
 	}
 	
 	@Test
