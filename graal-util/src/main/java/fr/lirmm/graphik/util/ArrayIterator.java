ce6733ae33feda6869043fdfe881f9d2dd0524c2
Clément SIPIETER <clement@6pi.fr>
2017-05-18 12:16:02 +0200

    array iterator

---
+++ graal-util/src/main/java/fr/lirmm/graphik/util/ArrayIterator.java
@@ -0,0 +1,28 @@
+package fr.lirmm.graphik.util;
+
+import java.util.Iterator;
+
+
+public class ArrayIterator<T> implements Iterator<T> {
+	public T[] array;
+	int i = 0;
+	
+	public ArrayIterator(T... array) {
+		this.array = array;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return i < array.length;
+	}
+
+	@Override
+	public T next() {
+		return array[i++];
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+}
