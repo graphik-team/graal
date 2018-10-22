138f6aca02e726e6990cd036389889cd95b37c0c
Olivier Rodriguez <olivier.rodriguez.66@hotmail.fr>
2018-10-22 15:43:07 +0200

    BasicChaseWithCompilation, correction of Generics

--- graal-forward-chaining/src/main/java/fr/lirmm/graphik/graal/forward_chaining/BasicChaseWithCompilation.java
+++ graal-forward-chaining/src/main/java/fr/lirmm/graphik/graal/forward_chaining/BasicChaseWithCompilation.java
@@ -1,28 +1,25 @@
 package fr.lirmm.graphik.graal.forward_chaining;
 
-import java.util.Iterator;
-
 import fr.lirmm.graphik.graal.api.core.AtomSet;
-import fr.lirmm.graphik.graal.api.core.ConjunctiveQuery;
+import fr.lirmm.graphik.graal.api.core.Query;
 import fr.lirmm.graphik.graal.api.core.Rule;
 import fr.lirmm.graphik.graal.api.forward_chaining.DirectRuleApplier;
 import fr.lirmm.graphik.graal.api.homomorphism.HomomorphismWithCompilation;
 import fr.lirmm.graphik.graal.core.compilation.IDCompilation;
-import fr.lirmm.graphik.graal.forward_chaining.BasicChase;
 import fr.lirmm.graphik.graal.forward_chaining.rule_applier.DefaultRuleApplierWithCompilation;
 
-public class BasicChaseWithCompilation<T extends AtomSet> extends BasicChase {
+public class BasicChaseWithCompilation<T extends AtomSet> extends BasicChase<T> {
 
 	public BasicChaseWithCompilation(Iterable<Rule> rules, T atomSet, IDCompilation compilation) {
-		super(rules, atomSet, new DefaultRuleApplierWithCompilation(compilation));
+		super(rules, atomSet, new DefaultRuleApplierWithCompilation<T>(compilation));
 	}
 
-	public BasicChaseWithCompilation(Iterable<Rule> rules, AtomSet atomSet, IDCompilation compilation,
-			HomomorphismWithCompilation<? super ConjunctiveQuery, ? super T> h) {
-		super(rules, atomSet, new DefaultRuleApplierWithCompilation(h, compilation));
+	public BasicChaseWithCompilation(Iterable<Rule> rules, T atomSet, IDCompilation compilation,
+			HomomorphismWithCompilation<? super Query, ? super T> h) {
+		super(rules, atomSet, new DefaultRuleApplierWithCompilation<T>(h, compilation));
 	}
 
-	public BasicChaseWithCompilation(Iterable<Rule> rules, AtomSet atomSet,
+	public BasicChaseWithCompilation(Iterable<Rule> rules, T atomSet,
 			DirectRuleApplier<? super Rule, ? super T> ruleApplier) {
 		super(rules, atomSet, ruleApplier);
 	}
