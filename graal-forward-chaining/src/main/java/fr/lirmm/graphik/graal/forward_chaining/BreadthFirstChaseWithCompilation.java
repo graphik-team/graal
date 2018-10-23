8a3772f110641f2c1cb99db23eaddb751ac05c7b
Olivier Rodriguez <olivier.rodriguez.66@hotmail.fr>
2018-10-23 17:29:15 +0200

    BreadthFirstChaseWithCompilation, correction of types

--- graal-forward-chaining/src/main/java/fr/lirmm/graphik/graal/forward_chaining/BreadthFirstChaseWithCompilation.java
+++ graal-forward-chaining/src/main/java/fr/lirmm/graphik/graal/forward_chaining/BreadthFirstChaseWithCompilation.java
@@ -1,27 +1,26 @@
 package fr.lirmm.graphik.graal.forward_chaining;
 
 import fr.lirmm.graphik.graal.api.core.AtomSet;
-import fr.lirmm.graphik.graal.api.core.ConjunctiveQuery;
+import fr.lirmm.graphik.graal.api.core.Query;
 import fr.lirmm.graphik.graal.api.core.Rule;
-import fr.lirmm.graphik.graal.api.forward_chaining.DirectRuleApplier;
+import fr.lirmm.graphik.graal.api.forward_chaining.RuleApplier;
 import fr.lirmm.graphik.graal.api.homomorphism.HomomorphismWithCompilation;
 import fr.lirmm.graphik.graal.core.compilation.IDCompilation;
 import fr.lirmm.graphik.graal.forward_chaining.rule_applier.DefaultRuleApplierWithCompilation;
 
-public class BreadthFirstChaseWithCompilation<T extends AtomSet> extends BreadthFirstChase {
-	
-	public BreadthFirstChaseWithCompilation(Iterable<Rule> rules, T atomSet, IDCompilation compilation) {
-		super(rules, atomSet, new DefaultRuleApplierWithCompilation(compilation));
+public class BreadthFirstChaseWithCompilation extends BreadthFirstChase {
+
+	public BreadthFirstChaseWithCompilation(Iterable<Rule> rules, AtomSet atomSet, IDCompilation compilation) {
+		super(rules, atomSet, new DefaultRuleApplierWithCompilation<AtomSet>(compilation));
 	}
 
 	public BreadthFirstChaseWithCompilation(Iterable<Rule> rules, AtomSet atomSet, IDCompilation compilation,
-			HomomorphismWithCompilation<? super ConjunctiveQuery, ? super T> h) {
-		super(rules, atomSet, new DefaultRuleApplierWithCompilation(h, compilation));
+			HomomorphismWithCompilation<Query, AtomSet> h) {
+		super(rules, atomSet, new DefaultRuleApplierWithCompilation<AtomSet>(h, compilation));
 	}
 
-//	public BreadthFirstChaseWithCompilation(Iterable<Rule> rules, AtomSet atomSet,
-//			DirectRuleApplier<? super Rule, ? super T> ruleApplier) {
-//		super(rules, atomSet, ruleApplier);
-//	}
-	
+	public BreadthFirstChaseWithCompilation(Iterable<Rule> rules, AtomSet atomSet,
+			RuleApplier<Rule, AtomSet> ruleApplier) {
+		super(rules, atomSet, ruleApplier);
+	}
 }
