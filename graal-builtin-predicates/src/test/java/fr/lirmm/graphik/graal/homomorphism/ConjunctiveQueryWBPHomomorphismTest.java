bfca83ea568087a3dd3b1abbc1e1069608a33d3c
Olivier Rodriguez <olivier.rodriguez.66@hotmail.fr>
2019-01-29 11:59:13 +0100

    graal-builtin-predicate, add some tests

--- graal-builtin-predicates/src/test/java/fr/lirmm/graphik/graal/homomorphism/ConjunctiveQueryWBPHomomorphismTest.java
+++ graal-builtin-predicates/src/test/java/fr/lirmm/graphik/graal/homomorphism/ConjunctiveQueryWBPHomomorphismTest.java
@@ -2,6 +2,11 @@ package fr.lirmm.graphik.graal.homomorphism;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -9,25 +14,36 @@ import org.junit.Test;
 import fr.lirmm.graphik.graal.api.core.AtomSet;
 import fr.lirmm.graphik.graal.api.core.Rule;
 import fr.lirmm.graphik.graal.api.core.RuleSet;
+import fr.lirmm.graphik.graal.api.core.Substitution;
+import fr.lirmm.graphik.graal.api.factory.TermFactory;
 import fr.lirmm.graphik.graal.api.forward_chaining.RuleApplier;
 import fr.lirmm.graphik.graal.api.util.ClassicBuiltInPredicates;
 import fr.lirmm.graphik.graal.converter.Object2RuleWithBuiltInPredicateConverter;
 import fr.lirmm.graphik.graal.core.DefaultBuiltInPredicateSet;
+import fr.lirmm.graphik.graal.core.DefaultConjunctiveQueryWithBuiltInPredicates;
+import fr.lirmm.graphik.graal.core.HashMapSubstitution;
 import fr.lirmm.graphik.graal.core.atomset.LinkedListAtomSet;
 import fr.lirmm.graphik.graal.core.ruleset.LinkedListRuleSet;
+import fr.lirmm.graphik.graal.core.term.DefaultTermFactory;
 import fr.lirmm.graphik.graal.forward_chaining.BreadthFirstChase;
 import fr.lirmm.graphik.graal.forward_chaining.rule_applier.RuleWithBuiltInPredicateApplier;
 import fr.lirmm.graphik.graal.homomorphism.checker.ConjunctiveQueryWithBuiltInPredicatesChecker;
 import fr.lirmm.graphik.graal.io.dlp.DlgpParser;
+import fr.lirmm.graphik.util.stream.CloseableIterator;
+import fr.lirmm.graphik.util.stream.CloseableIteratorAccumulator;
 
 public class ConjunctiveQueryWBPHomomorphismTest {
 	final static DefaultBuiltInPredicateSet btpredicates = new DefaultBuiltInPredicateSet(ClassicBuiltInPredicates.defaultPredicates());
 	final static RuleApplier<Rule, AtomSet> applier = new RuleWithBuiltInPredicateApplier<AtomSet>();
-
+	final static TermFactory termFactory = DefaultTermFactory.instance();
+	
+	private DefaultConjunctiveQueryWithBuiltInPredicates query;
 	private AtomSet facts;
 	private RuleSet rules;
 	private AtomSet result;
 	private AtomSet expected;
+	private List<Substitution> queryResult;
+	private List<Substitution> queryExpected;
 
 	@BeforeClass
 	public static void setUp() {
@@ -36,12 +52,16 @@ public class ConjunctiveQueryWBPHomomorphismTest {
 
 	@Before
 	public void setup() {
+		query = null;
 		facts = new LinkedListAtomSet();
 		rules = new LinkedListRuleSet();
 		result = new LinkedListAtomSet();
 		expected = new LinkedListAtomSet();
+		queryResult = new ArrayList<>();
+		queryExpected = new ArrayList<>();
 	}
 
+	// ========================================================================
 	public void expected(String... atoms) throws Exception {
 
 		for (String atom : atoms)
@@ -52,7 +72,7 @@ public class ConjunctiveQueryWBPHomomorphismTest {
 		Object2RuleWithBuiltInPredicateConverter converter = new Object2RuleWithBuiltInPredicateConverter(btpredicates);
 
 		for (String query : queries)
-			rules.add((Rule)converter.convert(DlgpParser.parseRule(query)));
+			rules.add((Rule) converter.convert(DlgpParser.parseRule(query)));
 	}
 
 	public void facts(String... atoms) throws Exception {
@@ -64,6 +84,35 @@ public class ConjunctiveQueryWBPHomomorphismTest {
 	private void checkResult() throws Exception {
 		boolean res = expected.equals(result);
 		assertTrue("Expected result:\n" + expected + "\nHave:\n" + result, res);
+	}
+
+	// ========================================================================
+
+	private void query(String query) throws Exception {
+		this.query = new DefaultConjunctiveQueryWithBuiltInPredicates(DlgpParser.parseQuery(query), btpredicates);
+	}
+
+	private void queryExpected(Substitution... subs) throws Exception {
+
+		for (Substitution s : subs)
+			queryExpected.add(s);
+	}
+
+	private void checkQueryResult() throws Exception {
+		boolean res = queryExpected.equals(queryResult);
+		assertTrue("Expected result:\n" + queryExpected + "\nHave:\n" + queryResult, res);
+	}
+
+	private Substitution makeSubstitution(Object... keyVal) {
+		Substitution ret = new HashMapSubstitution();
+		Iterator<Object> it = Arrays.asList(keyVal).iterator();
+
+		// A key MUST be followed by a value
+		while (it.hasNext())
+			//For now we assume that the objects are both String, and conserve the Object typing for future evolution.
+			ret.put(termFactory.createVariable(it.next()), termFactory.createConstant(it.next()));
+
+		return ret;
 	}
 
 	@Test
@@ -96,5 +145,44 @@ public class ConjunctiveQueryWBPHomomorphismTest {
 		bf.execute();
 
 		checkResult();
+	}
+
+	@SuppressWarnings("resource")
+	@Test
+	public void goodQuery_0() throws Exception {
+		facts( //
+				"p(a).", //
+				"p(b).", //
+				"h(a,b).", //
+				"h(a,a).", //
+				"h(b,a)." //
+		);
+		queryExpected( //
+				makeSubstitution("X", "a") //
+		);
+		query("?(X) :- h(X,Y), bt__eq(X,Y).");
+
+		CloseableIterator<Substitution> res = SmartHomomorphism.instance().execute(query, facts);
+		queryResult = new CloseableIteratorAccumulator<Substitution>(res).consumeAll().getList();
+		checkQueryResult();
+	}
+
+	@SuppressWarnings("resource")
+	@Test
+	public void goodQuery_1() throws Exception {
+		facts( //
+				"h(a,b).", //
+				"x(b,c).", //
+				"h(c,d).", //
+				"x(d,d)." //
+		);
+		queryExpected( //
+				makeSubstitution("X", "c") //
+		);
+		query("?(X) :- h(X,Y), x(Y,Z), bt__eq(Y,Z).");
+
+		CloseableIterator<Substitution> res = SmartHomomorphism.instance().execute(query, facts);
+		queryResult = new CloseableIteratorAccumulator<Substitution>(res).consumeAll().getList();
+		checkQueryResult();
 	}
 }
